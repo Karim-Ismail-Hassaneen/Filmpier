00ccf4f06d31dc913141a6e2ad655f7d6fd54858
Karim-Ismail-Hassaneen <121400836+Karim-Ismail-Hassaneen@users.noreply.github.com>
2024-01-05 04:10:17 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='w-full h-screen text-blue-700 flex items-center justify-center text-4xl'>
+        Hello
+       </div>
+    </>
+  )
+}
+
+export default App
